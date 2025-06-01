276b2c8a84d5dca152fc04631915f29240f6c271
Pavel Shukhman <taleodor@gmail.com>
2025-06-01 14:00:52 -0400

    chore: bump copyright date in approve release

--- cmd/approveRelease.go
+++ cmd/approveRelease.go
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2020 - 2024 Reliza Incorporated (Reliza (tm), https://reliza.io)
+Copyright (c) 2020 - 2025 Reliza Incorporated (Reliza (tm), https://reliza.io)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
