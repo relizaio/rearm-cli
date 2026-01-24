917f065c9bacfe8e42ad896b42b8ce0bc1e3e8d8
Pavel Shukhman <taleodor@gmail.com>
2026-01-23 19:20:48 -0500

    feat: update bear integration to new version of bear

--- cmd/bear.go
+++ cmd/bear.go
@@ -18,46 +18,167 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 
 	cdx "github.com/CycloneDX/cyclonedx-go"
-	"github.com/machinebox/graphql"
 	"github.com/spf13/cobra"
 )
 
 var (
-	bearUri string
+	bearUri    string
+	bearApiKey string
 )
 
-var resolveBearSupplierCmd = &cobra.Command{
+const bearBatchSize = 20
+
+var enrichSupplierCmd = &cobra.Command{
 	Use:   "enrichsupplier",
-	Short: "Resolve Supplier on BEAR",
-	Long:  `For each component, resolve its supplier where not present on given BEAR instance`,
+	Short: "Enrich Supplier on BEAR",
+	Long:  `For each component, enrich its supplier where not present using BEAR`,
 	Run: func(cmd *cobra.Command, args []string) {
-		resolveBearSupplierFunc()
+		enrichSupplierFunc()
+	},
+}
+
+var enrichLicenseCmd = &cobra.Command{
+	Use:   "enrichlicense",
+	Short: "Enrich License on BEAR",
+	Long:  `For each component, enrich its license where missing or contains LicenseRef-scancode using BEAR`,
+	Run: func(cmd *cobra.Command, args []string) {
+		enrichLicenseFunc()
+	},
+}
+
+var enrichCmd = &cobra.Command{
+	Use:   "enrich",
+	Short: "Enrich Supplier and License on BEAR",
+	Long:  `For each component, enrich supplier (if missing) and license (if missing or contains LicenseRef-scancode) using BEAR`,
+	Run: func(cmd *cobra.Command, args []string) {
+		enrichFunc()
 	},
 }
 
 func init() {
-	resolveBearSupplierCmd.PersistentFlags().StringVar(&bearUri, "bearUri", "https://beardemo.rearmhq.com", "BEAR URI to use")
-	bomUtils.AddCommand(resolveBearSupplierCmd)
+	enrichSupplierCmd.PersistentFlags().StringVar(&bearUri, "bearUri", "https://beardemo.rearmhq.com", "BEAR URI to use")
+	enrichSupplierCmd.PersistentFlags().StringVar(&bearApiKey, "bearApiKey", "", "BEAR API Key")
+	bomUtils.AddCommand(enrichSupplierCmd)
+
+	enrichLicenseCmd.PersistentFlags().StringVar(&bearUri, "bearUri", "https://beardemo.rearmhq.com", "BEAR URI to use")
+	enrichLicenseCmd.PersistentFlags().StringVar(&bearApiKey, "bearApiKey", "", "BEAR API Key")
+	bomUtils.AddCommand(enrichLicenseCmd)
+
+	enrichCmd.PersistentFlags().StringVar(&bearUri, "bearUri", "https://beardemo.rearmhq.com", "BEAR URI to use")
+	enrichCmd.PersistentFlags().StringVar(&bearApiKey, "bearApiKey", "", "BEAR API Key")
+	bomUtils.AddCommand(enrichCmd)
 }
 
-func resolveBearSupplierFunc() {
+type BearGraphQLRequest struct {
+	Query string `json:"query"`
+}
+
+type BearLicense struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	URL  string `json:"url,omitempty"`
+}
+
+type BearLicenseChoice struct {
+	License    *BearLicense `json:"license,omitempty"`
+	Expression string       `json:"expression,omitempty"`
+}
+
+type BearAddress struct {
+	Country             string `json:"country,omitempty"`
+	Region              string `json:"region,omitempty"`
+	Locality            string `json:"locality,omitempty"`
+	PostOfficeBoxNumber string `json:"postOfficeBoxNumber,omitempty"`
+	PostalCode          string `json:"postalCode,omitempty"`
+	StreetAddress       string `json:"streetAddress,omitempty"`
+}
+
+type BearContact struct {
+	Name  string `json:"name,omitempty"`
+	Email string `json:"email,omitempty"`
+	Phone string `json:"phone,omitempty"`
+}
+
+type BearSupplier struct {
+	Name    string        `json:"name,omitempty"`
+	Address *BearAddress  `json:"address,omitempty"`
+	URL     []string      `json:"url,omitempty"`
+	Contact []BearContact `json:"contact,omitempty"`
+}
+
+type BearComponent struct {
+	Type     string              `json:"type,omitempty"`
+	Name     string              `json:"name,omitempty"`
+	Purl     string              `json:"purl,omitempty"`
+	Supplier *BearSupplier       `json:"supplier,omitempty"`
+	Licenses []BearLicenseChoice `json:"licenses,omitempty"`
+}
+
+type BearEnrichBatchResponse struct {
+	Data struct {
+		EnrichBatch []BearComponent `json:"enrichBatch"`
+	} `json:"data"`
+}
+
+func enrichSupplierFunc() {
 	fmt.Println("Using BEAR at", bearUri)
 
 	bom := readBom()
-
 	components := bom.Components
+	if components == nil {
+		fmt.Println("No components found in BOM")
+		return
+	}
+
+	// Collect components that need supplier enrichment
+	var purlsToEnrich []string
+	purlToIndices := make(map[string][]int)
+
 	for ind, comp := range *components {
-		if nil == comp.Supplier {
-			fmt.Println("Processing purl", comp.PackageURL)
-			supplier := bearGqlSupplierRoutine(comp.PackageURL)
-			(*components)[ind].Supplier = &supplier
+		if comp.Supplier == nil && comp.PackageURL != "" {
+			purlsToEnrich = append(purlsToEnrich, comp.PackageURL)
+			purlToIndices[comp.PackageURL] = append(purlToIndices[comp.PackageURL], ind)
 		}
 	}
+
+	if len(purlsToEnrich) == 0 {
+		fmt.Println("No components need supplier enrichment")
+		writeOutput(bom)
+		return
+	}
+
+	fmt.Printf("Found %d components needing supplier enrichment\n", len(purlsToEnrich))
+
+	// Process in batches
+	enrichedComponents := bearEnrichBatch(purlsToEnrich)
+
+	// Update components with enriched supplier data
+	suppliersEnriched := 0
+	for _, enriched := range enrichedComponents {
+		if indices, ok := purlToIndices[enriched.Purl]; ok {
+			supplier := convertBearSupplierToCdx(enriched.Supplier)
+			if supplier != nil {
+				for _, idx := range indices {
+					(*components)[idx].Supplier = supplier
+					suppliersEnriched++
+				}
+			}
+		}
+	}
+
+	if debug == "true" {
+		fmt.Printf("Enrichment statistics: %d suppliers enriched\n", suppliersEnriched)
+	}
+
 	bom.Components = components
 	outErr := writeOutput(bom)
 	if outErr != nil {
@@ -66,35 +187,310 @@ func resolveBearSupplierFunc() {
 	}
 }
 
-func bearGqlSupplierRoutine(purl string) cdx.OrganizationalEntity {
-	req := graphql.NewRequest(`
-		mutation resolveSupplier($purl: String!) {
-			resolveSupplier(purl: $purl) {
-				name
-				address {
-					country
-					region
-					locality
-					postOfficeBoxNumber
-					postalCode
-					streetAddress
-				}
-				url
-				contact {
-					name
-					email
-					phone
+func enrichLicenseFunc() {
+	fmt.Println("Using BEAR at", bearUri)
+
+	bom := readBom()
+	components := bom.Components
+	if components == nil {
+		fmt.Println("No components found in BOM")
+		return
+	}
+
+	// Collect components that need license enrichment
+	var purlsToEnrich []string
+	purlToIndices := make(map[string][]int)
+
+	for ind, comp := range *components {
+		if comp.PackageURL != "" && needsLicenseEnrichment(&comp) {
+			purlsToEnrich = append(purlsToEnrich, comp.PackageURL)
+			purlToIndices[comp.PackageURL] = append(purlToIndices[comp.PackageURL], ind)
+		}
+	}
+
+	if len(purlsToEnrich) == 0 {
+		fmt.Println("No components need license enrichment")
+		writeOutput(bom)
+		return
+	}
+
+	fmt.Printf("Found %d components needing license enrichment\n", len(purlsToEnrich))
+
+	// Process in batches
+	enrichedComponents := bearEnrichBatch(purlsToEnrich)
+
+	// Update components with enriched license data
+	licensesEnriched := 0
+	for _, enriched := range enrichedComponents {
+		if indices, ok := purlToIndices[enriched.Purl]; ok {
+			licenses := convertBearLicensesToCdx(enriched.Licenses)
+			if licenses != nil && len(*licenses) > 0 {
+				for _, idx := range indices {
+					(*components)[idx].Licenses = licenses
+					licensesEnriched++
 				}
 			}
 		}
-	`)
-	req.Var("purl", purl)
-	res := sendRequestWithUri(req, "resolveSupplier", bearUri+"/graphql")
-	var cdxContact cdx.OrganizationalEntity
-	parseError := json.Unmarshal([]byte(res), &cdxContact)
-	if parseError != nil {
-		fmt.Println(parseError)
+	}
+
+	if debug == "true" {
+		fmt.Printf("Enrichment statistics: %d licenses enriched\n", licensesEnriched)
+	}
+
+	bom.Components = components
+	outErr := writeOutput(bom)
+	if outErr != nil {
+		fmt.Println(outErr)
 		os.Exit(1)
 	}
-	return cdxContact
+}
+
+func enrichFunc() {
+	fmt.Println("Using BEAR at", bearUri)
+
+	bom := readBom()
+	components := bom.Components
+	if components == nil {
+		fmt.Println("No components found in BOM")
+		return
+	}
+
+	// Collect components that need enrichment (supplier OR license)
+	var purlsToEnrich []string
+	purlToIndices := make(map[string][]int)
+	needsSupplier := make(map[string]bool)
+	needsLicense := make(map[string]bool)
+
+	for ind, comp := range *components {
+		if comp.PackageURL == "" {
+			continue
+		}
+
+		compNeedsSupplier := comp.Supplier == nil
+		compNeedsLicense := needsLicenseEnrichment(&comp)
+
+		if compNeedsSupplier || compNeedsLicense {
+			if _, exists := purlToIndices[comp.PackageURL]; !exists {
+				purlsToEnrich = append(purlsToEnrich, comp.PackageURL)
+			}
+			purlToIndices[comp.PackageURL] = append(purlToIndices[comp.PackageURL], ind)
+			if compNeedsSupplier {
+				needsSupplier[comp.PackageURL] = true
+			}
+			if compNeedsLicense {
+				needsLicense[comp.PackageURL] = true
+			}
+		}
+	}
+
+	if len(purlsToEnrich) == 0 {
+		fmt.Println("No components need enrichment")
+		writeOutput(bom)
+		return
+	}
+
+	fmt.Printf("Found %d components needing enrichment\n", len(purlsToEnrich))
+
+	// Process in batches
+	enrichedComponents := bearEnrichBatch(purlsToEnrich)
+
+	// Update components with enriched data
+	suppliersEnriched := 0
+	licensesEnriched := 0
+	for _, enriched := range enrichedComponents {
+		if indices, ok := purlToIndices[enriched.Purl]; ok {
+			for _, idx := range indices {
+				// Only update supplier if it was missing
+				if needsSupplier[enriched.Purl] {
+					supplier := convertBearSupplierToCdx(enriched.Supplier)
+					if supplier != nil {
+						(*components)[idx].Supplier = supplier
+						suppliersEnriched++
+					}
+				}
+				// Only update license if it was missing or contained LicenseRef-scancode
+				if needsLicense[enriched.Purl] {
+					licenses := convertBearLicensesToCdx(enriched.Licenses)
+					if licenses != nil && len(*licenses) > 0 {
+						(*components)[idx].Licenses = licenses
+						licensesEnriched++
+					}
+				}
+			}
+		}
+	}
+
+	if debug == "true" {
+		fmt.Printf("Enrichment statistics: %d suppliers enriched, %d licenses enriched\n", suppliersEnriched, licensesEnriched)
+	}
+
+	bom.Components = components
+	outErr := writeOutput(bom)
+	if outErr != nil {
+		fmt.Println(outErr)
+		os.Exit(1)
+	}
+}
+
+func needsLicenseEnrichment(comp *cdx.Component) bool {
+	// Missing license
+	if comp.Licenses == nil || len(*comp.Licenses) == 0 {
+		return true
+	}
+
+	// Check if any license contains LicenseRef-scancode
+	for _, lic := range *comp.Licenses {
+		if lic.Expression != "" && strings.Contains(lic.Expression, "LicenseRef-scancode") {
+			return true
+		}
+		if lic.License != nil {
+			if strings.Contains(lic.License.ID, "LicenseRef-scancode") {
+				return true
+			}
+			if strings.Contains(lic.License.Name, "LicenseRef-scancode") {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func bearEnrichBatch(purls []string) []BearComponent {
+	var allResults []BearComponent
+
+	// Process in batches of bearBatchSize
+	for i := 0; i < len(purls); i += bearBatchSize {
+		end := i + bearBatchSize
+		if end > len(purls) {
+			end = len(purls)
+		}
+		batch := purls[i:end]
+
+		fmt.Printf("Processing batch %d-%d of %d\n", i+1, end, len(purls))
+
+		results := bearEnrichBatchRequest(batch)
+		allResults = append(allResults, results...)
+	}
+
+	return allResults
+}
+
+func bearEnrichBatchRequest(purls []string) []BearComponent {
+	// Build the GraphQL query with purls array
+	purlsJson, _ := json.Marshal(purls)
+	query := fmt.Sprintf(`{"query":"mutation { enrichBatch(purls: %s) { type name purl supplier { name address { country region locality postOfficeBoxNumber postalCode streetAddress } url contact { name email phone } } licenses { license { id name url } expression } } }"}`,
+		strings.ReplaceAll(string(purlsJson), `"`, `\"`))
+
+	req, err := http.NewRequest("POST", bearUri+"/graphql", bytes.NewBufferString(query))
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+		return nil
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	if bearApiKey != "" {
+		req.Header.Set("X-API-Key", bearApiKey)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error making request: %v\n", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response: %v\n", err)
+		return nil
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error response from BEAR: %s\n", string(body))
+		return nil
+	}
+
+	var response BearEnrichBatchResponse
+	if err := json.Unmarshal(body, &response); err != nil {
+		fmt.Printf("Error parsing response: %v\n", err)
+		return nil
+	}
+
+	return response.Data.EnrichBatch
+}
+
+func convertBearSupplierToCdx(supplier *BearSupplier) *cdx.OrganizationalEntity {
+	if supplier == nil || supplier.Name == "" {
+		return nil
+	}
+
+	entity := &cdx.OrganizationalEntity{
+		Name: supplier.Name,
+	}
+
+	if len(supplier.URL) > 0 {
+		entity.URL = &supplier.URL
+	}
+
+	if supplier.Address != nil {
+		entity.Address = &cdx.PostalAddress{
+			Country:             supplier.Address.Country,
+			Region:              supplier.Address.Region,
+			Locality:            supplier.Address.Locality,
+			PostOfficeBoxNumber: supplier.Address.PostOfficeBoxNumber,
+			PostalCode:          supplier.Address.PostalCode,
+			StreetAddress:       supplier.Address.StreetAddress,
+		}
+	}
+
+	if len(supplier.Contact) > 0 {
+		var contacts []cdx.OrganizationalContact
+		for _, c := range supplier.Contact {
+			contacts = append(contacts, cdx.OrganizationalContact{
+				Name:  c.Name,
+				Email: c.Email,
+				Phone: c.Phone,
+			})
+		}
+		entity.Contact = &contacts
+	}
+
+	return entity
+}
+
+func convertBearLicensesToCdx(licenses []BearLicenseChoice) *cdx.Licenses {
+	if len(licenses) == 0 {
+		return nil
+	}
+
+	var cdxLicenses cdx.Licenses
+	for _, lic := range licenses {
+		if lic.Expression != "" {
+			cdxLicenses = append(cdxLicenses, cdx.LicenseChoice{
+				Expression: lic.Expression,
+			})
+		} else if lic.License != nil {
+			cdxLicense := &cdx.License{}
+			if lic.License.ID != "" {
+				cdxLicense.ID = lic.License.ID
+			}
+			if lic.License.Name != "" {
+				cdxLicense.Name = lic.License.Name
+			}
+			if lic.License.URL != "" {
+				cdxLicense.URL = lic.License.URL
+			}
+			cdxLicenses = append(cdxLicenses, cdx.LicenseChoice{
+				License: cdxLicense,
+			})
+		}
+	}
+
+	if len(cdxLicenses) == 0 {
+		return nil
+	}
+
+	return &cdxLicenses
 }
